baeb4fc188a7deae463c7233b9c3f6b832bd34d5
Abigail Cabunoc <abigail.cabunoc@gmail.com>
2018-03-26 13:24:23 -0400

    script updates! add coaches

--- scripts/google_form_projects.js
+++ scripts/google_form_projects.js
@@ -15,27 +15,118 @@ function Initialize() {
 
 }
 
+function getCoachCount(data, keys) {
+  var counts = {};
+  for (var i=0; i < keys.length; i++) {
+    if(keys[i] == "Coach"){
+      for(var j=0; j < data.length; j++) {
+        var cellData = data[j][i];
+        if(isCellEmpty_(cellData)){
+          continue;
+        }
+        if(counts[cellData]){
+          counts[cellData]++;
+        } else {
+          counts[cellData] = 1;
+        }
+      }
+    }
+  }
+  return counts;
+}
+
+
+// Returns true if the cell where cellData was read from is empty.
+// Arguments:
+//   - cellData: string
+function isCellEmpty_(cellData) {
+  return typeof(cellData) == "string" && cellData == "";
+}
+
+
+function selectCoach(tracks, coachCounts){
+
+  var allCoaches = {
+    "Openness": [
+      {"github": "@quirksahern & @fimakeswork", "email":"abby@mozillafoundation.org", "name":"Sam & Fi"},
+      {"github": "@tpoisot", "email":"abby@mozillafoundation.org", "name":"Tim"},
+      {"github": "@gilbertslade", "email":"abby@mozillafoundation.org", "name":"Gilbert"},
+      {"github": "@SamanthaHindle & @aariops", "email":"abby@mozillafoundation.org", "name":"Rute"},
+      {"github": "@chadsansing", "email":"abby@mozillafoundation.org", "name":"Chad"}
+      ],
+    "Web Literacy": [
+      {"github": "@anmechung", "email":"abby@mozillafoundation.org", "name":"An-Me"},
+      {"github": "@jamjwait", "email":"abby@mozillafoundation.org", "name":"Janice"},
+      {"github": "@Wiroll & @psubhashish", "email":"abby@mozillafoundation.org", "name":"Craig & Subha"}
+    ],
+    "Internet Privacy": [
+      {"github": "@veerskyfire", "email":"abby@mozillafoundation.org", "name":"Viral"},
+      {"github": "@princiya", "email":"abby@mozillafoundation.org", "name":"Princiya"},
+    ],
+    "Digital Inclusion": [
+      {"github": "@cbaba20", "email":"abby@mozillafoundation.org", "name":"Chandan"},
+      {"github": "@meghanlazier", "email":"abby@mozillafoundation.org", "name":"Meghan"},
+      {"github": "@erindcole", "email":"abby@mozillafoundation.org", "name":"Erin"},
+      {"github": "---", "email":"abby@mozillafoundation.org", "name":"Leah"},
+    ],
+    "Decentralization": [
+      {"github": "@aj-ptw", "email":"abby@mozillafoundation.org", "name":"AJ"}
+    ]
+  };
+
+  var coaches = [];
+  for(var i = 0; i<tracks.length; i++){
+    var c = allCoaches[tracks[i]];
+    if(c){
+      coaches = coaches.concat(c);
+    }
+  }
+
+  var coach;
+  var lowestCount;
+  for(var j = 0; j<coaches.length; j++) {
+    var curr = coachCounts[coaches[j]["name"]];
+    if(!curr) { curr = 0 };
+    if(!coach || curr < lowestCount){
+      lowestCount = curr;
+      coach = coaches[j];
+      Logger.log("current lowest: " + lowestCount);
+      Logger.log("current coach: " + coach["name"]);
+    }
+  }
+  Logger.log(coach);
+  return coach;
+}
 
 function SendGoogleForm(e)
 {
   var getTrack = {
   "WEB LITERACY: Projects that teach individuals skills to shape — and not simply consume — the web": "Web Literacy",
   "OPENNESS: Projects that enshrine the technologies that run the web as transparent and understandable, and allow anyone to invent online without asking permission": "Openness",
-  "PRIVACY & SECURITY: Projects that illuminate what happens to our personal data online, and how to make the Internet safer for all": "Privacy + Security",
+  "PRIVACY & SECURITY: Projects that illuminate what happens to our personal data online, and how to make the Internet safer for all": "Internet Privacy",
   "DIGITAL INCLUSION: Projects that ensure everyone has an equal opportunity to access the Internet, and can use it to improve their lives and societies": "Digital Inclusion",
   "DECENTRALIZATION: Projects that protect and secure an Internet controlled by many, so that no one actor can own it or control it or switch it off": "Decentralization"
   };
+
   var template_start = "%3C%21---%20Keep%20everything%20below%20and%20click%20%27Submit%20new%20issue%27%20%20---%3E%0D%0A%0D%0A%2A%2A%5B%20Project%20Contact%20%5D%2A%2A%20";
   var template_contribute = "%2A%2A%2A%0D%0A%0D%0A%23%23%20Want%20to%20contribute%20to%20this%20project%20during%20%23mozsprint%3F%0D%0AJoin%20us%20at%20the%20Global%20Sprint%2C%20May%2010-11.%20Leave%20a%20comment%20below%20if%20you%27re%20interested%20in%20contributing%20to%20this%20project%20during%20%23mozsprint%202018%21%0D%0A%0D%0A%2A%2A%2A%0D%0A%0D%0A%23%23%20Note%20to%20the%20Project%20Lead%20%3Atada%3A%0D%0ACongrats%2C%20";
-  var template_notes_to_project_lead = "%21%20This%20is%20your%20official%20project%20listing%20for%20the%20Mozilla%20Global%20Sprint%202018.%20To%20confirm%20your%20registration%2C%20please%20complete%20and%20check%20off%20the%20following%3A%0D%0A%0D%0A-%20%5B%20%5D%20Complete%20%5BOpen%20Leadership%20101%5D%28https%3A%2F%2Fmozilla.teachable.com%2Fp%2Fopen-leadership-101%29%0D%0A-%20%5B%20%5D%20Provide%20a%20GitHub%20repository%20for%20work%20and%20discussion%20on%20your%20project%20in%20a%20comment%0D%0A%20%20-%20new%20to%20GitHub%3F%20%20Here%27s%20a%20%5Bstep-by-step%20guide%20on%20using%20the%20%23mozsprint%20template%5D%28https%3A%2F%2Fmozilla.github.io%2Fglobal-sprint%2Fproject-lead%2Ftemplates%2F%29%0D%0A-%20%5B%20%5D%20Create%20a%20README%20file%20in%20your%20project%20repository.%20This%20file%20should%20help%20newcomers%20understand%20what%20your%20project%20is%2C%20why%20it%27s%20important%2C%20and%20kinds%20of%20help%20you%27re%20looking%20for.%0D%0A-%20%5B%20%5D%20%5BCreate%20file%3A%20LICENSE%5D%28http%3A%2F%2Fchoosealicense.com%2F%29%20to%20give%20your%20project%20an%20open%20license%2C%20allowing%20for%20sharing%20and%20remixing.%0D%0A-%20%5B%20%5D%20Turn%20on%20your%20%5BIssue%20Tracker%5D%28https%3A%2F%2Fmozilla.github.io%2Fglobal-sprint%2Fproject-lead%2Ftemplates%2F%234-create-issues%29%20and%20create%20at%20least%20three%20issues%20to%20describe%20each%20task%20that%20you%20need%20help%20with%20and%20how%20a%20contributor%20can%20get%20started%20on%20that%20task.%0D%0A-%20%5B%20%5D%20%5BCreate%20a%20label%5D%28https%3A%2F%2Fmozilla.github.io%2Fglobal-sprint%2Fproject-lead%2Ftemplates%2F%235-add-a-mozsprint-label%29%20called%20%60mozsprint%60%20and%20apply%20it%20to%20your%20issues.%0D%0A%0D%0A%23%23%23%23%20Checklist%20for%20FEATURED%20Projects%20%3Aclipboard%3A%0D%0ATo%20have%20your%20project%20FEATURED%20on%20%5BMozilla%20Pulse%5D%28http%3A%2F%2Fmozillapulse.org%2F%29%2C%20complete%20the%20following%20documentation.%20In%20past%20Sprints%2C%20well-documented%20featured%20projects%20have%205%20times%20more%20contributions%20than%20other%20projects.%20Details%20about%20each%20item%20and%20more%20information%20about%20how%20to%20create%20them%20are%20%5Bon%20our%20Project%20Requirements%20Page%5D%28https%3A%2F%2Fmozilla.github.io%2Fglobal-sprint%2Fproject-lead%2Fproject-requirements%2F%29.%0D%0A%0D%0A%2A%20%5B%20%5D%20In%20your%20README%2C%20link%20to%20the%20%5BMozilla%20Community%20Participation%20Guidelines%5D%28https%3A%2F%2Fwww.mozilla.org%2Fen-US%2Fabout%2Fgovernance%2Fpolicies%2Fparticipation%2F%29%20or%20write%20your%20own%20code%20of%20conduct.%0D%0A%2A%20%5B%20%5D%20Create%20file%3A%20CONTRIBUTING.md%20so%20others%20know%20how%20they%20can%20contribute.%20If%20you%27d%20like%2C%20you%20can%20%5Bremix%20this%20template%5D%28https%3A%2F%2Fgithub.com%2Facabunoc%2Fmozsprint-repo-template%2Fblob%2Fmaster%2FCONTRIBUTING.md%29%0D%0A%2A%20%5B%20%5D%20%5BFill%20out%20this%20form%5D%28https%3A%2F%2Fwww.mozillapulse.org%2Fadd%29%20to%20submit%20your%20project%20to%20Mozilla%20Pulse.%20Add%20the%20tags%20%60mozsprint%60%20and%20%602018%60.%0D%0A%0D%0AOnce%20all%20of%20the%20above%20is%20complete%2C%0D%0A-%20%5B%20%5D%20Leave%20a%20comment%20with%20the%20text%20%60This%20is%20ready%20to%20be%20featured%20on%20Mozilla%20Pulse%60.%20Your%20wrangler%20will%20review%20this%20issue%20and%20approve%20your%20project%20if%20everything%20looks%20good%20%3Aballoon%3A%0D%0A%0D%0AIf%20you%20get%20stuck%20at%20any%20point%2C%20feel%20free%20to%20look%20at%20the%20%5Brequirements%20page%5D%28https%3A%2F%2Fmozilla.github.io%2Fglobal-sprint%2Fproject-lead%2Fproject-requirements%2F%29%20and%20%5Bproject%20templates%5D%28https%3A%2F%2Fmozilla.github.io%2Fglobal-sprint%2Fproject-lead%2Ftemplates%2F%29.%20We%27re%20here%20to%20help%20you%20through%20this%20process.";
+  var template_pl_a = "%21+This+is+your+official+project+listing+for+the+Mozilla+Global+Sprint+2018.+To+confirm+your+registration%2C+please+complete+and+check+off+the+following%3A%0D%0A%0D%0A-+%5B";
+  var template_pl_b = "%5D+Complete+%5BOpen+Leadership+101%5D%28https%3A%2F%2Fmozilla.teachable.com%2Fp%2Fopen-leadership-101%29%0D%0A-+%5B";
+  var template_pl_c = "%5D+Provide+a+GitHub+repository+for+work+and+discussion+on+your+project+%28in+a+comment+or+at+the+top+of+this+issue%29%0D%0A";
+  var template_pl_d = "++-+new+to+GitHub%3F++Here%27s+a+%5Bstep-by-step+guide+on+using+the+%23mozsprint+template%5D%28https%3A%2F%2Fmozilla.github.io%2Fglobal-sprint%2Fproject-lead-guide%2Ftemplates%2F%29%0D%0A";
+  var template_pl_e = "-+%5B+%5D+%5BCreate+a+README+file%5D%28https%3A%2F%2Fmozilla.github.io%2Fopen-leadership-training-series%2Farticles%2Fopening-your-project%2Fwrite-a-great-project-readme%2F%29+in+your+project+repository.+This+file+should+help+newcomers+understand+what+your+project+is%2C+why+it%27s+important%2C+and+kinds+of+help+you%27re+looking+for.%0D%0A-+%5B+%5D+%5BCreate+file%3A+LICENSE%5D%28http%3A%2F%2Fchoosealicense.com%2F%29+to+give+your+project+an+open+license%2C+allowing+for+sharing+and+remixing.%0D%0A-+%5B+%5D+Turn+on+your+%5BIssue+Tracker%5D%28https%3A%2F%2Fmozilla.github.io%2Fglobal-sprint%2Fproject-lead-guide%2Ftemplates%2F%234-create-issues%29+and+create+at+least+three+issues+to+describe+each+task+that+you+need+help+with+and+how+a+contributor+can+get+started+on+that+task.%0D%0A-+%5B+%5D+%5BCreate+a+label%5D%28https%3A%2F%2Fmozilla.github.io%2Fglobal-sprint%2Fproject-lead-guide%2Ftemplates%2F%235-add-a-mozsprint-label%29+called+%60mozsprint%60+and+apply+it+to+your+issues.%0D%0A%0D%0A%23%23%23%23+Checklist+for+FEATURED+Projects+%3Aclipboard%3A%0D%0ATo+have+your+project+FEATURED+on+%5BMozilla+Pulse%5D%28http%3A%2F%2Fmozillapulse.org%2F%29%2C+complete+the+following+documentation.+In+past+Sprints%2C+well-documented+featured+projects+have+5+times+more+contributions+than+other+projects.+Details+about+each+item+and+more+information+about+how+to+create+them+are+%5Bon+our+Project+Requirements+Page%5D%28https%3A%2F%2Fmozilla.github.io%2Fglobal-sprint%2Fproject-lead-guide%2Fproject-requirements%2F%29.%0D%0A%0D%0A%2A+%5B+%5D+In+your+README%2C+link+to+the+%5BMozilla+Community+Participation+Guidelines%5D%28https%3A%2F%2Fwww.mozilla.org%2Fen-US%2Fabout%2Fgovernance%2Fpolicies%2Fparticipation%2F%29+or+%5Bwrite+your+own+code+of+conduct%5D%28https%3A%2F%2Fmozilla.github.io%2Fopen-leadership-training-series%2Farticles%2Fbuilding-communities-of-contributors%2Fwrite-a-code-of-conduct%2F%29.%0D%0A%2A+%5B+%5D+Create+file%3A+CONTRIBUTING.md+so+others+know+how+they+can+contribute.+If+you%27d+like%2C+you+can+%5Bremix+this+template%5D%28https%3A%2F%2Fgithub.com%2Facabunoc%2Fmozsprint-repo-template%2Fblob%2Fmaster%2FCONTRIBUTING.md%29%0D%0A%2A+%5B+%5D+%5BFollow+these+instructions%5D%28https%3A%2F%2Fmozilla.github.io%2Fglobal-sprint%2Fproject-lead-guide%2Ffeatured-projects%2F%29+to+submit+your+project+to+Mozilla+Pulse.%0D%0A%0D%0AOnce+all+of+the+above+is+complete%2C%0D%0A-+%5B+%5D+Leave+a+comment+with+the+text+%60This+is+ready+to+be+featured+on+Mozilla+Pulse%60.+";
+  var template_pl_f = "+will+review+this+issue+and+approve+your+project+if+everything+looks+good+%3Aballoon%3A%0D%0A%0D%0AIf+you+get+stuck+at+any+point%2C+feel+free+to+look+at+the+%5Brequirements+page%5D%28https%3A%2F%2Fmozilla.github.io%2Fglobal-sprint%2Fproject-lead-guide%2Fproject-requirements%2F%29+and+%5Bproject+templates%5D%28https%3A%2F%2Fmozilla.github.io%2Fglobal-sprint%2Fproject-lead-guide%2Ftemplates%2F%29";
+  var template_pl_g = ".+We%27re+here+to+help+you+through+this+process.";
+
   try
   {
     // You may also replace this with another email address
     var email = "abby@mozillafoundation.org"; //Session.getActiveUser().getEmail();
 
-
     var s = SpreadsheetApp.getActiveSheet();
     var headers = s.getRange(1,1,1,s.getLastColumn()).getValues()[0];
+    var dataRange = s.getRange(s.getFrozenRows()+1, 1, s.getMaxRows(), s.getMaxColumns());
+
 
     var reply = e.namedValues['Email'].toString();
     var project = e.namedValues['Project Title'].toString();
@@ -59,6 +150,17 @@ function SendGoogleForm(e)
       }
     })
     track = tracks.join(', ');
+    var mentored = e.namedValues['Mozilla Open Leaders'];
+    if(mentored) { mentored = mentored.toString(); }
+    var coach;
+    if (mentored != "Yes") {
+      coach = selectCoach(tracks, getCoachCount(dataRange.getValues(), headers));
+      Logger.log(coach);
+      var coachCell = s.getRange("Q" + row);
+      coachCell.setValue(coach["name"]);
+    }
+    var ol101 = e.namedValues['Have you taken the short (1-hour) online course Open Leadership 101?'];
+    if(ol101) { ol101 = ol101.toString(); }
 
     var location = e.namedValues['What is your physical location and time zone?'];
     if(location) { location = location.toString(); }
@@ -71,7 +173,17 @@ function SendGoogleForm(e)
       + encodeURIComponent("\n\n### Description\n" + description + "\n")
       + template_contribute
       + encodeURIComponent(github)
-      + template_notes_to_project_lead;
+      + template_pl_a;
+    body += (ol101 == "Yes! I have taken Open Leadership 101") ? "x" : "+";
+    body += template_pl_b;
+    body += link ? "x" : "+";
+    body += template_pl_c;
+    body += link ? template_pl_d : "";
+    body += template_pl_e;
+    body += coach ? encodeURIComponent("Your Project Coach ") :  encodeURIComponent("@acabunoc or @zee-moz ") ;
+    body += template_pl_f;
+    body += coach ? encodeURIComponent(" or reach out to your Project Coach, " + coach["github"]) : "";
+    body += template_pl_g;
 
     var link_base = "https://github.com/mozilla/global-sprint/issues/new?title=" + encodeURIComponent(project);
     var link = link_base + "&body=" + body;
@@ -82,10 +194,22 @@ function SendGoogleForm(e)
     var message = "Dear " + name + ",";
 
     message += "<p>Thank you for submitting your open project, " + project + ", to Mozilla's Global Sprint 2018 (#mozsprint). ";
-    message += "To complete your registraion, you'll need to</p><p>1) Log in to <a href=\"https://github.com/\">GitHub</a>. You can register for free if you don't have an account.</p><p>2) Follow <a href='" + link + "'>this unique link</a> and click on 'Create new issue' to generate a listing for your project:</p>";
+    message += "To complete your registration, you'll need to</p><p>1) Log in to <a href=\"https://github.com/\">GitHub</a>. You can register for free if you don't have an account.</p><p>2) Follow <a href='" + link + "'>this unique link</a> and click on 'Create new issue' to generate a listing for your project:</p>";
     message +="<p><br /><a href='" + link + "' style='font-size:14pt;background:#3bb7ef;padding:10px 15px;color:black;text-decoration:none;margin:10px 0;'>Create Project Listing</a><br /><br /></p>"
-    message += "<p>If you run into trouble, check out our <a href='https://mozilla.github.io/global-sprint/project-lead-guide/'>Guide for Project Leads</a>. As the sprint nears, you'll be assigned a \"project wrangler\" -- someone who will help you prepare and answer any questions you may have about your project at the Sprint. So keep an eye on your inbox and your GitHub issue tabs for more information!</p><p>If you have any questions that aren't answered there, feel free to reach out -- email us at <a href='mailto:globalsprint@mozillafoundation.org'>globalsprint@mozillafoundation.org</a>. We're here to help! </p>";
-    message += "<p>See you at #mozsprint!<br />Abby and the Global Sprint team</p><br /><hr><br />";
+    message += "<p>If you run into trouble, check out our <a href='https://mozilla.github.io/global-sprint/project-lead-guide/'>Guide for Project Leads</a>."
+    if(coach) {
+      message += " " +  coach["name"] + " (cc’ed) will be your Project Coach during the Sprint. Your Project Coach will help you prepare your project and answer any questions you may have."
+    }
+    message += "</p><p>If you have any questions that aren't answered there, feel free to reach out -- email us at <a href='mailto:globalsprint@mozillafoundation.org'>globalsprint@mozillafoundation.org</a>";
+    if(coach){
+      message += " or reach out to your Project Coach";
+    }
+    message += ". We're here to help! </p>";
+    message += "<p>See you at #mozsprint!<br />Abby";
+    if(coach){
+      message += ", " + coach["name"];
+    }
+    message += " and the Global Sprint team</p><br /><hr><br />";
     message += "<p style='font-size:small;color:#666'>Link above didn't work? Make you're logged into GitHub before logging in. Full url here: " + link + "<br /><br />";
     message += "Still have problems? Reply to this email and we'll help you out!</p>"
 
@@ -93,11 +217,16 @@ function SendGoogleForm(e)
     // This is the MailApp service of Google Apps Script
     // that sends the email. You can also use GmailApp here.
 
-    MailApp.sendEmail({
+    var mailConfig = {
      to: reply,
      subject: subject,
      htmlBody: message
-    });
+    };
+    if(coach){
+      mailConfig["cc"] = coach["email"];
+    }
+
+    MailApp.sendEmail(mailConfig);
 
   } catch (e) {
     Logger.log(e.toString());
